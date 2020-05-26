4e49dc1da36751cfc574109c82120bc35a2ac2a2
Lucas Veras <lucasdsveras@gmail.com>
2020-05-26 17:09:47 +0100

    Add documentation

--- R/bmi.R
+++ R/bmi.R
@@ -1,3 +1,28 @@
+#' Compute body mass index (BMI)
+#'
+#' \code{bmi} returns the row-wise BMI in kilograms per meter squared.
+#'
+#' @param data The data frame or tibble with the data to be computed.
+#'
+#' @param mass,height The bare (unquoted) names of the columns with body mass
+#'   and height data. `mass` unit must be kilograms and `height` unit must be
+#'   meters. If the `height` unit is centimeters, it is converted to meters
+#'   before BMI computation and a warning is shown.
+#'
+#' @param category If TRUE, the \code{\link[=bmi_cat]{bmi_cat()}} function is called to
+#'   classify the BMI category, returning the classification in another column
+#'   as a factor; otherwise (the default) BMI category is not classified.
+#'
+#' @export
+#'
+#' @seealso \code{\link[=bmi_cat]{bmi_cat()}}
+#'
+#' @examples
+#' df <- dplyr::starwars %>% 
+#'   # Converting height to meters
+#'   dplyr::mutate(height = height / 100)
+#' 
+#' bmi(df, mass, height, category = TRUE)
 bmi <- function(data, mass, height, category = FALSE) {
   mass <- rlang::enquo(mass)
   height <- rlang::enquo(height)
@@ -48,6 +73,33 @@ bmi <- function(data, mass, height, category = FALSE) {
   data
 }
 
+#' Classify body mass index (BMI) category
+#'
+#' \code{bmi_cat} returns the row-wise BMI category as factor with 6 levels:
+#' \itemize{
+#'   \item Underweight (18.5 < BMI)
+#'   \item Normal weight (18.5 \eqn{\le} BMI < 25)
+#'   \item Overweight (25 \eqn{\le} BMI < 30)
+#'   \item Obesity class I (30 \eqn{\le} BMI < 35)
+#'   \item Obesity class II (35 \eqn{\le} BMI < 40)
+#'   \item Obesity class III (40 \eqn{\le} BMI)
+#' }
+#'
+#' @param data The data frame or tibble with the data to be computed.
+#' 
+#' @param bmi The bare (unquoted) name of the column with BMI data.
+#' 
+#' @export
+#'
+#' @seealso \code{\link[=bmi]{bmi()}}
+#'
+#' @examples
+#' df <- dplyr::starwars %>% 
+#'   # Converting height to meters
+#'   dplyr::mutate(height = height / 100) %>% 
+#'   bmi(mass, height)
+#' 
+#' bmi_cat(df, bmi)
 bmi_cat <- function(data, bmi) {
   bmi <- rlang::enquo(bmi)
 
