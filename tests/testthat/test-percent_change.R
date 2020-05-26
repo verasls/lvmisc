8b012b1d4bac40f655d47cae04c05cf74bf6b264
Lucas Veras <lucasdsveras@gmail.com>
2020-05-26 11:37:15 +0100

    Remove test for argument  and check for the class of the error

--- tests/testthat/test-percent_change.R
+++ tests/testthat/test-percent_change.R
@@ -19,27 +19,6 @@ test_that("percent_change works", {
   expect_equal(out, c(0, 0.5, 5, 50, 100, - 0.5, - 5, - 50, - 100, NA))
 })
 
-test_that("rounding works", {
-  df <- data.frame(
-   change = c(1.0114, 1.0116),
-   bl = 10
-  )
-  df$fu <- df$bl * df$change
-
-  out <- percent_change(df, bl, fu, 1)
-  out <- out[["percent_change"]]
-
-  expect_equal(out, c(1.1, 1.2))
-
-  df$change <- c(1.0001, 1.0099)
-  df$fu <- df$bl * df$change
-
-  out <- percent_change(df, bl, fu, 3)
-  out <- out[["percent_change"]]
-
-  expect_equal(out, c(0.01, 0.99))
-})
-
 test_that("error handling works", {
   df <- data.frame(
     chr = letters[1:10],
@@ -49,18 +28,22 @@ test_that("error handling works", {
 
   expect_error(
     percent_change(df, chr, nmr),
-    "`baseline` must be numeric; not character."
+    "`baseline` must be numeric; not character.",
+    class = "error_argument_type"
   )
   expect_error(
     percent_change(df, lgc, nmr),
-    "`baseline` must be numeric; not logical."
+    "`baseline` must be numeric; not logical.",
+    class = "error_argument_type"
   )
   expect_error(
     percent_change(df, nmr, chr),
-    "`followup` must be numeric; not character."
+    "`followup` must be numeric; not character.",
+    class = "error_argument_type"
   )
   expect_error(
     percent_change(df, nmr, lgc),
-    "`followup` must be numeric; not logical."
+    "`followup` must be numeric; not logical.",
+    class = "error_argument_type"
   )
 })
