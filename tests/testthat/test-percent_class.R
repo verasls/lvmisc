c6f2dfa2af6a28cee50130ec061f9109216fc46e
Lucas Veras <lucasdsveras@gmail.com>
2020-05-27 14:57:55 +0100

    Initial commit

--- tests/testthat/test-percent_class.R
+++ tests/testthat/test-percent_class.R
@@ -6,4 +6,21 @@ test_that("lvmisc_percent prints correctly", {
     x <- c(runif(9), NA) 
     percent(x)
   })
+
+  x <- percent(0.5)
+  
+  expect_equal(vec_ptype_abbr(x), "prcnt")
+})
+
+test_that("vector casting works", {
+  x <- percent(0.25)
+  y <- percent(0.50)
+  z <- 0.75
+
+  expect_s3_class(vec_c(x, y), c("lvmisc_percent", "vctrs_vctr"), exact = TRUE)
+  expect_type(vec_c(x, z), "double")
+  expect_type(vec_c(z, x), "double")
+  expect_s3_class(as_percent(z), c("lvmisc_percent", "vctrs_vctr"), exact = TRUE)
+  expect_equal(is_percent(x), TRUE)
+  expect_equal(is_percent(z), FALSE)
 })
