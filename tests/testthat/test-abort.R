7a939cf905ec97a8dc06164fb637b5dece49ec1f
Lucas Veras <lucasdsveras@gmail.com>
2020-05-27 10:09:20 +0100

    Add tests for abort_argument_length()

--- tests/testthat/test-abort.R
+++ tests/testthat/test-abort.R
@@ -5,9 +5,23 @@ test_that("abort_argument_type works", {
   err <- rlang::catch_cnd(
     abort_argument_type("x", must = "be numeric", not = x)
   )
+
   expect_s3_class(err, "error_argument_type")
   expect_equal(err$message, "`x` must be numeric; not character.")
   expect_equal(err$arg, "x")
   expect_equal(err$must, "be numeric")
   expect_equal(err$not, "character")
+})
+
+test_that("abort_argument_length works", {
+  x <- 1:10
+  err <- rlang::catch_cnd(
+    abort_argument_length("x", must = "have length 1", not = x)
+  )
+
+  expect_s3_class(err, "error_argument_length")
+  expect_equal(err$message, "`x` must have length 1; not 10.")
+  expect_equal(err$arg, "x")
+  expect_equal(err$must, "have length 1")
+  expect_equal(err$not, 10)
 })
