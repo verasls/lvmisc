b0e09bbfb0b784bc0d7ff3829f7a6bef39773e92
Lucas Veras <lucasdsveras@gmail.com>
2020-06-03 11:06:24 +0100

    Initial commit

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,22 @@
+context("utils")
+
+test_that("tb works", {
+  1 + 1
+  out <- tb()
+
+  expect_s3_class(out, "rlang_trace")
+})
+
+test_that("pa works", {
+  verify_output(test_path("test-utils-pa.txt"), {
+    df <- dplyr::starwars
+    pa(df)
+  })
+})
+
+test_that("lu and lna works", {
+  x <- c(1, 1, 2, 3, 3, 3, 4, 4, NA, NA)
+
+  expect_equal(lu(x), 4)
+  expect_equal(lna(x), 2)
+})
