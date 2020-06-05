83f56ee66d22b5be81c9076c5b900093f9ad40cd
Lucas Veras <lucasdsveras@gmail.com>
2020-06-05 17:48:32 +0100

    Initial commit

---
+++ tests/testthat/test-divide_by_quantile.R
@@ -0,0 +1,30 @@
+context("divide_by_quantile")
+
+test_that("divide_by_quantile works", {
+  x <- c(1:9, NA)
+  out <- divide_by_quantile(x, 3)
+
+  expect_is(out, "factor")
+  expect_equal(
+    out,
+    factor(c(1, 1, 1, 2, 2, 2, 3, 3, 3, NA))
+  )
+})
+
+test_that("error handling works", {
+  expect_error(
+    divide_by_quantile(letters, 3),
+    "`data` must be numeric; not character.",
+    class = "error_argument_type"
+  )
+  expect_error(
+    divide_by_quantile(1:10, 4.5),
+    "`n` must be interger; not double.",
+    class = "error_argument_type"
+  )
+  expect_error(
+    divide_by_quantile(1:10, 4, na.rm = "true"),
+    "`na.rm` must be logical; not character.",
+    class = "error_argument_type"
+  )
+})
