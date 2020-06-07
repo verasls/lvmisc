e9100310ad78316b6852ac42bf34b5c50dba29aa
Lucas Veras <lucasdsveras@gmail.com>
2020-06-07 18:52:16 +0100

    Initial commit

---
+++ R/center_variable.R
@@ -0,0 +1,34 @@
+#' Center variable
+#'
+#' Center a variable by subtracting the mean from each element. Centering can
+#'   be performed by the grand mean when \code{by = NULL} (the default), or by
+#'   group means when \code{by} is a factor variable.
+#'
+#' @param variable A numeric vector.
+#'
+#' @param by A vector with the \code{factor} class.
+#'
+#' @export
+#'
+#' @examples
+#' df <- data.frame(
+#'   id = 1:20,
+#'   group = as.factor(sample(c("A", "B"), 20, replace = TRUE)),
+#'   body_mass = rnorm(20, mean = 65, sd = 12)
+#' )
+#'
+#' df$body_mass_centered <- center_variable(df$body_mass, df$group)
+center_variable <- function(variable, by = NULL) {
+  if (!is.numeric(variable)) {
+    abort_argument_type("variable", must = "be numeric", not = variable)
+  }
+  if (!is.factor(by)) {
+    abort_argument_type("by", must = "be factor", not = by)
+  }
+
+  if (is.null(by)) {
+    as.numeric(scale(variable, scale = FALSE))
+  } else {
+    as.numeric(variable - tapply(variable, by, mean, na.rm = TRUE)[by])
+  }
+}
