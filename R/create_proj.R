23d4a80569cf56735a523f0468736570e1db1239
Lucas <lucasdsveras@gmail.com>
2020-11-26 09:47:17 +0000

    Add an argument to specify the sub-directories to be created

--- R/create_proj.R
+++ R/create_proj.R
@@ -1,4 +1,4 @@
-create_proj <- function(path) {
+create_proj <- function(path, sub_dirs = "default") {
   if (dir.exists(path)) {
     usethis::ui_stop("{usethis::ui_path(path)} already exists")
   } else if (file.exists(path)) {
@@ -11,11 +11,13 @@ create_proj <- function(path) {
     "Creating project top-level directory {usethis::ui_path(path)}"
   )
 
-  sub_dirs <- c("code/", "data/", "docs/", "figs/", "tabs/")
+  if (sub_dirs == "default") {
+    sub_dirs <- c("code/", "data/", "docs/", "figs/", "tabs/")
+  }
   purrr::walk(sub_dirs, ~ create_sub_dir(path, .x))
 }
 
 create_sub_dir <- function(main_dir, sub_dir) {
   dir.create(paste0(main_dir, "/", sub_dir))
-  usethis::ui_done("Creating sub-directory {usethis::ui_path(sub_dir)}")
+  usethis::ui_done("Creating sub-directory {sub_dir}")
 }
