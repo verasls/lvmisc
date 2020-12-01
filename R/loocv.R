f786247ba8d36c98db9eda1aa8bee10bf8cc2b5d
Lucas <lucasdsveras@gmail.com>
2020-12-01 16:51:40 +0000

    Initial commit

---
+++ R/loocv.R
@@ -0,0 +1,33 @@
+#' @export
+loocv <- function(model, data, id, keep = "used") {
+  UseMethod("loocv")
+}
+
+#' @export
+loocv.lm <- function(model, data, id, keep = "used") {
+  formula <- stats::formula(model)
+  outcome <- as.character(rlang::f_lhs(formula))
+
+  loocv_split <- rsample::group_vfold_cv(data, group = id)
+  training_data <- purrr::map(loocv_split$splits, get_training_data)
+  testing_data <- purrr::map(loocv_split$splits, get_testing_data)
+
+  trained_models <- purrr::map(training_data, ~ lm(formula, data = .x))
+  predicted <- purrr::map2(
+    trained_models, testing_data,
+    ~ stats::predict(.x, newdata = .y, allow.new.levels = TRUE)
+  )
+  predicted <- unname(purrr::as_vector(predicted))
+  actual <- purrr::as_vector(purrr::map(testing_data, outcome))
+
+  if (keep == "used") {
+    tibble::tibble(data[id], actual, predicted)
+  } else if (keep == "none") {
+    tibble::tibble(actual, predicted)
+  } else if (keep == "all") {
+    tibble::as_tibble(cbind(data, actual, predicted))
+  }
+}
+
+get_training_data <- function(x) rsample::analysis(x)
+get_testing_data <- function(x) rsample::assessment(x)
