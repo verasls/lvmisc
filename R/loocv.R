99919d800c9cd72b48033523641956d575b7d46c
Lucas <lucasdsveras@gmail.com>
2020-12-03 11:56:31 +0000

    Add a method for lmerMod class

--- R/loocv.R
+++ R/loocv.R
@@ -58,6 +58,54 @@ loocv.lm <- function(model, data, id, keep = "used") {
   }
 }
 
+#' @rdname loocv
+#' @export
+loocv.lmerMod <- function(model, data, id, keep = "used") {
+  id_col_name <- rlang::as_string(rlang::ensym(id))
+  data_name <- rlang::as_string(rlang::ensym(data))
+  if (length(class(model)) > 1) {
+    classes <- class(model)[class(model) != "lm"]
+    abort_class_not_implemented("loocv", classes)
+  }
+  if (!is.data.frame(data)) {
+    abort_argument_type(arg = "data", must = "be data.frame", not = data)
+  }
+  if (id_col_name %!in% names(data)) {
+    abort_column_not_found(data = data_name, col_name = id_col_name)
+  }
+
+  id <- rlang::enquo(id)
+  formula <- stats::formula(model)
+  outcome <- as.character(rlang::f_lhs(formula))
+
+  loocv_split <- rsample::group_vfold_cv(data, group = id)
+  training_data <- purrr::map(loocv_split$splits, get_training_data)
+  testing_data <- purrr::map(loocv_split$splits, get_testing_data)
+
+  trained_models <- purrr::map(
+    training_data,
+    ~ lme4::lmer(
+      formula, data = .x,
+      REML = grepl("REML", summary(model)$methTitle)
+    )
+  )
+  predicted <- purrr::map2(
+    trained_models, testing_data,
+    ~ stats::predict(.x, newdata = .y, allow.new.levels = TRUE)
+  )
+  predicted <- unname(purrr::as_vector(predicted))
+  actual <- purrr::as_vector(purrr::map(testing_data, outcome))
+
+  if (keep == "used") {
+    id <- rlang::as_string(rlang::ensym(id))
+    new_loocv(tibble::tibble(data[id], actual, predicted))
+  } else if (keep == "none") {
+    new_loocv(tibble::tibble(actual, predicted))
+  } else if (keep == "all") {
+    new_loocv(tibble::as_tibble(cbind(data, actual, predicted)))
+  }
+}
+
 get_training_data <- function(x) rsample::analysis(x)
 get_testing_data <- function(x) rsample::assessment(x)
 
