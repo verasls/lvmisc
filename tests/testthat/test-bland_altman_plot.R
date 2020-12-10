d1e8dd0ba7cae53092dbf92886bc2bf20856c009
Lucas <lucasdsveras@gmail.com>
2020-12-10 20:02:59 -0300

    Test visual output of all classes supported by bland_altman_plot()

--- tests/testthat/test-bland_altman_plot.R
+++ tests/testthat/test-bland_altman_plot.R
@@ -1,10 +1,19 @@
 test_that("output of bland_altman_plot() is stable", {
   mtcars <- tibble::as_tibble(mtcars, rownames = "car")
-  m <- stats::lm(disp ~ mpg, mtcars)
-  cv <- loocv(m, mtcars, car)
+  m1 <- stats::lm(disp ~ mpg, mtcars)
+  m2 <- lme4::lmer(disp ~ mpg + (1 | gear), mtcars)
+  cv <- loocv(m1, mtcars, car)
 
   vdiffr::expect_doppelganger(
-    "Bland Altman plot",
+    "Bland Altman plot - cv",
     bland_altman_plot(cv, colour = as.factor(am))
+  )
+  vdiffr::expect_doppelganger(
+    "Bland Altman plot - lm",
+    bland_altman_plot(m1)
+  )
+  vdiffr::expect_doppelganger(
+    "Bland Altman plot - lmerMod",
+    bland_altman_plot(m2)
   )
 })
