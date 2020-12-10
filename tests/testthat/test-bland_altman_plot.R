dcda188e1d14a31cb863e0d25f02b57dcecad252
Lucas <lucasdsveras@gmail.com>
2020-12-10 20:12:12 -0300

    Test that error handling works

--- tests/testthat/test-bland_altman_plot.R
+++ tests/testthat/test-bland_altman_plot.R
@@ -1,3 +1,33 @@
+test_that("error handling works", {
+  df <- data.frame(
+    subj = 1:30,
+    y = sample.int(30, replace = TRUE),
+    x = sample(c("A", "B"), 30, replace = TRUE, prob = c(0.6, 0.4))
+  )
+  m <- stats::glm(y ~ x, df, family = poisson())
+
+  expect_error(
+    bland_altman_plot(m),
+    glue::glue(
+      "The method `model_data` is not yet implemented for an object \\
+      of class `glm`.
+      If you would like it to be implemented, please file an issue at \\
+      https://github.com/verasls/lvmisc/issues."
+    ),
+    class = "error_no_method_for_class"
+  )
+  expect_error(
+    bland_altman_plot("m"),
+    glue::glue(
+      "The method `model_data` is not yet implemented for an object \\
+      of class `character`.
+      If you would like it to be implemented, please file an issue at \\
+      https://github.com/verasls/lvmisc/issues."
+    ),
+    class = "error_no_method_for_class"
+  )
+})
+
 test_that("output of bland_altman_plot() is stable", {
   mtcars <- tibble::as_tibble(mtcars, rownames = "car")
   m1 <- stats::lm(disp ~ mpg, mtcars)
