d4550b4a393f447aca9f482671d45820a6eeb4dc
Lucas <lucasdsveras@gmail.com>
2020-12-11 23:50:53 -0300

    Test visual output

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -20,3 +20,43 @@ test_that("error handling works", {
     class = "error_argument_value"
   )
 })
+
+test_that("output of plot_scatter() is stable", {
+  vdiffr::expect_doppelganger(
+    "Scatterplot",
+    plot_scatter(mtcars, disp, mpg, color = factor(cyl))
+  )
+})
+
+test_that("output of plot_line() is stable", {
+  vdiffr::expect_doppelganger(
+    "Line plot",
+    plot_line(Orange, age, circumference, colour = Tree)
+  )
+})
+
+test_that("output of plot_hist() is stable", {
+  vdiffr::expect_doppelganger(
+    "Histogram - NULL",
+    plot_hist(iris, Petal.Width)
+  )
+  vdiffr::expect_doppelganger(
+    "Histogram - Sturges",
+    plot_hist(iris, Petal.Width, bin_width = "Sturges")
+  )
+  vdiffr::expect_doppelganger(
+    "Histogram - scott",
+    plot_hist(iris, Petal.Width, bin_width = "scott")
+  )
+  vdiffr::expect_doppelganger(
+    "Histogram - FD",
+    plot_hist(iris, Petal.Width, bin_width = "FD")
+  )
+})
+
+test_that("output of plot_qq() is stable", {
+  vdiffr::expect_doppelganger(
+    "QQ plot",
+    plot_qq(mtcars, mpg)
+  )
+})
