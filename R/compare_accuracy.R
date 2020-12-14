1d1ff8bcc463b5bb3c4b533b1519933f320aaf17
Lucas <lucasdsveras@gmail.com>
2020-12-14 11:55:10 -0300

    Change how model_classes are extracted

--- R/compare_accuracy.R
+++ R/compare_accuracy.R
@@ -1,11 +1,13 @@
 compare_accuracy <- function(...) {
   models <- list(...)
-  return(models)
-
   model_names <- as.character(match.call(expand.dots = FALSE)$`...`)
-  model_classes <- purrr::map_chr(models, class)
+
+  model_accuracy <- purrr::map(models, accuracy)
+  model_classes <- purrr::map(model_accuracy, attributes)
+  model_classes <- purrr::map_chr(model_classes, "model_class")
+
+  model_accuracy <- purrr::map_dfr(model_accuracy, cbind)
   model_info <- data.frame(Model = model_names, Class = model_classes)
-  model_accuracy <- purrr::map_dfr(models, accuracy)
 
   cbind(model_info, model_accuracy)
 }
