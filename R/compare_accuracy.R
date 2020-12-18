4e45d1faa69991c6a8fac5ec9cf50e4fefcb9a7c
Lucas <lucasdsveras@gmail.com>
2020-12-18 18:13:13 -0300

    Refit lmerMod models using ML

--- R/compare_accuracy.R
+++ R/compare_accuracy.R
@@ -3,6 +3,9 @@ compare_accuracy <- function(..., rank_by = "none", quiet = FALSE) {
   models <- list(...)
   models_name <- as.character(match.call(expand.dots = FALSE)$`...`)
 
+  needs_refit <- purrr::map_lgl(models, get_fit_method)
+  models <- purrr::map2(needs_refit, models, refit_lmerMod)
+
   models_accuracy <- purrr::map(models, accuracy)
   models_class <- purrr::map(models_accuracy, attributes)
   models_class <- purrr::map_chr(models_class, "model_class")
@@ -11,7 +14,7 @@ compare_accuracy <- function(..., rank_by = "none", quiet = FALSE) {
   models_info <- data.frame(Model = models_name, Class = models_class)
 
   if (isFALSE(quiet)) {
-    warn_compare_accuracy(models, models_class)
+    warn_compare_accuracy(models, models_class, needs_refit)
   }
 
   compare <- cbind(models_info, models_accuracy)
@@ -22,7 +25,7 @@ compare_accuracy <- function(..., rank_by = "none", quiet = FALSE) {
   }
 }
 
-warn_compare_accuracy <- function(models, models_class) {
+warn_compare_accuracy <- function(models, models_class, needs_refit) {
   models_response <- purrr::map2(models, models_class, get_model_response)
   same_response <- all(
     purrr::map_lgl(models_response, ~ identical(.x, models_response[[1]]))
@@ -37,6 +40,10 @@ warn_compare_accuracy <- function(models, models_class) {
   if (isFALSE(same_class)) {
     rlang::warn("Not all models are of the same class.")
   }
+
+  if (any(needs_refit)) {
+    rlang::warn("Some models were refit using maximum likelihood.")
+  }
 }
 
 get_model_response <- function(model, model_class) {
@@ -49,4 +56,24 @@ get_model_response <- function(model, model_class) {
 
   model_data <- stats::model.frame(model)
   model_data[[outcome]]
+}
+
+get_fit_method <- function(model) {
+  if ("lmerMod" %in% class(model)) {
+    REML <- grepl("REML", summary(model)$methTitle)
+  } else {
+    REML <- FALSE
+  }
+  REML
+}
+
+refit_lmerMod <- function(needs_refit, model) {
+  if (isTRUE(needs_refit)) {
+    model_formula <- stats::formula(model)
+    model_data <- stats::model.frame(model)
+
+    lme4::lmer(model_formula, model_data, REML = FALSE)
+  } else {
+    model
+  }
 }
