68fc82e237ce9a3a18302b34f65701bc3f7681dd
Lucas <lucasdsveras@gmail.com>
2020-12-19 17:46:05 -0300

    Test different classes of models

--- tests/testthat/test-compare_accuracy.R
+++ tests/testthat/test-compare_accuracy.R
@@ -16,3 +16,67 @@ test_that("error handling works", {
     class = "error_argument_value"
   )
 })
+
+test_that("compare_accuracy() works for models of class lm", {
+  m1 <- lm(Sepal.Length ~ Species, data = iris)
+  m2 <- lm(Sepal.Length ~ Species + Petal.Length, data = iris)
+  out <- compare_accuracy(m1, m2)
+
+  col_names <- c(
+    "Model", "Class", "R2", "R2_adj", "AIC", "BIC", "MAE", "MAPE", "RMSE"
+  )
+  expect_equal(names(out), col_names)
+  expect_equal(out[["Class"]], c("lm", "lm"))
+})
+
+test_that("compare_accuracy() works for models of class lmerMod", {
+  m1 <- lme4::lmer(
+    Sepal.Length ~ Sepal.Width + (1 | Species), data = iris, REML = FALSE
+  )
+  m2 <- lme4::lmer(
+    Sepal.Length ~ Sepal.Width + Petal.Length + (1 | Species), data = iris
+  )
+  out <- compare_accuracy(m1, m2, quiet = TRUE)
+
+  col_names <- c(
+    "Model", "Class", "R2_marg", "R2_cond", "AIC", "BIC", "MAE", "MAPE", "RMSE"
+  )
+  expect_equal(names(out), col_names)
+  expect_equal(out[["Class"]], c("lmerMod", "lmerMod"))
+})
+
+test_that("compare_accuracy() works for models of class lvmisc_cv", {
+  mtcars <- tibble::as_tibble(mtcars, rownames = "cars")
+  m1 <- lm(disp ~ mpg, mtcars)
+  m2 <- lm(disp ~ mpg + hp, mtcars)
+  cv1 <- loo_cv(m1, mtcars, cars)
+  cv2 <- loo_cv(m2, mtcars, cars)
+  out <- compare_accuracy(cv1, cv2, quiet = TRUE)
+
+  col_names <- c(
+    "Model", "Class", "R2", "R2_adj", "AIC", "BIC", "MAE", "MAPE", "RMSE"
+  )
+  expect_equal(names(out), col_names)
+  expect_equal(out[["Class"]], c("lvmisc_cv_model/lm", "lvmisc_cv_model/lm"))
+})
+
+test_that("compare_accuracy() works for models of different classes", {
+  mtcars <- tibble::as_tibble(mtcars, rownames = "cars")
+  m1 <- lm(Sepal.Length ~ Species, data = iris)
+  m2 <- lme4::lmer(
+    Sepal.Length ~ Sepal.Width + Petal.Length + (1 | Species), data = iris
+  )
+  m3 <- lm(disp ~ mpg * hp, mtcars)
+  cv3 <- loo_cv(m3, mtcars, cars)
+  out <- compare_accuracy(m1, m2, cv3, quiet = TRUE)
+
+  col_names <- c(
+    "Model", "Class", "R2", "R2_adj", "R2_marg", "R2_cond",
+    "AIC", "BIC", "MAE", "MAPE", "RMSE"
+  )
+  expect_equal(names(out), col_names)
+  expect_equal(
+    out[["Class"]],
+    c("lm", "lmerMod", "lvmisc_cv_model/lm")
+  )
+})
