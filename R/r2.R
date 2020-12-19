6efbfefc80cd032c3f77808d20051860967982f1
Lucas <lucasdsveras@gmail.com>
2020-12-19 15:17:57 -0300

    Rename output variables and refer to var() as stats::var()

--- R/r2.R
+++ R/r2.R
@@ -2,16 +2,16 @@ r2 <- function(model) {
   model_matrix <- stats::model.matrix(model)
 
   var_mod <- unclass(lme4::VarCorr(model))
-  var_fix <- var(as.vector(lme4::fixef(model) %*% t(model_matrix)))
+  var_fix <- stats::var(as.vector(lme4::fixef(model) %*% t(model_matrix)))
   var_ran <- sum(
     purrr::map_dbl(var_mod, ~ compute_var_ran(.x, model_matrix))
   )
   var_res <- attributes(lme4::VarCorr(model))$sc ^ 2
 
-  marg <- var_fix / (var_fix + var_ran + var_res)
-  cond <- (var_fix + var_ran) / (var_fix + var_ran + var_res)
+  R2_marg <- var_fix / (var_fix + var_ran + var_res)
+  R2_cond <- (var_fix + var_ran) / (var_fix + var_ran + var_res)
 
-  data.frame(marg, cond)
+  data.frame(R2_marg, R2_cond)
 }
 
 compute_var_ran <- function(var_mod, model_matrix) {
