23c59ac7a0ebb774cf56cc8c90f052807b83af60
Lucas <lucasdsveras@gmail.com>
2020-12-19 15:40:25 -0300

    Make r2() an S3 generic and implement default, lm and lmerMod methods

--- R/r2.R
+++ R/r2.R
@@ -1,4 +1,25 @@
 r2 <- function(model) {
+  UseMethod("r2")
+}
+
+r2.default <- function(model) {
+  msg <- glue::glue(
+    "If you would like it to be implemented, please file an issue at \\
+    https://github.com/verasls/lvmisc/issues."
+  )
+  abort_no_method_for_class("r2", class(model), msg)
+}
+
+r2.lm <- function(model) {
+  check_args_r2(model)
+  R2 <- summary(model)[["r.squared"]]
+  R2_adj <- summary(model)[["adj.r.squared"]]
+
+  data.frame(R2, R2_adj)
+}
+
+r2.lmerMod <- function(model) {
+  check_args_r2(model)
   model_matrix <- stats::model.matrix(model)
 
   var_mod <- unclass(lme4::VarCorr(model))
@@ -17,4 +38,15 @@ r2 <- function(model) {
 compute_var_ran <- function(var_mod, model_matrix) {
   z <- as.matrix(model_matrix[, rownames(var_mod), drop = FALSE])
   sum(rowSums((z %*% var_mod) * z)) / nrow(model_matrix)
+}
+
+check_args_r2 <- function(model) {
+  if ("lvmisc_cv" %!in% class(model) & length(class(model)) > 1) {
+    classes <- class(model)[class(model) %!in% c("lm", "lmerMod")]
+    msg <- glue::glue(
+      "If you would like it to be implemented, please file an issue at \\
+      https://github.com/verasls/lvmisc/issues."
+    )
+    abort_no_method_for_class("accuracy", classes, msg)
+  }
 }
