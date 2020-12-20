e708896f6603345351cd485d1ef2018b22a35603
Lucas <lucasdsveras@gmail.com>
2020-12-20 02:04:38 -0300

    Check arguments

--- R/vif.R
+++ R/vif.R
@@ -27,6 +27,8 @@ vif.lmerMod <- function(model) {
 }
 
 compute_vif <- function(model) {
+  model_terms <- labels(stats::terms(model))
+  check_args_vif(model, model_terms)
   var_cov <- as.matrix(stats::vcov(model))
   model_assign <- attributes(stats::model.matrix(model))[["assign"]]
 
@@ -34,14 +36,7 @@ compute_vif <- function(model) {
     var_cov <- var_cov[- 1, - 1]
     model_assign <- model_assign[- 1]
   } else {
-    rlang::warn("Model has no intercept; VIFs may not be sensible.")
-  }
-
-  model_terms <- labels(stats::terms(model))
-  if (length(model_terms) < 2) {
-    rlang::abort(
-      "Not enought terms in the model to check for multicollinearity."
-    )
+    rlang::warn("`model` has no intercept; VIFs may not be sensible.")
   }
 
   r <- stats::cov2cor(var_cov)
@@ -54,6 +49,22 @@ compute_vif <- function(model) {
   names(vif) <- model_terms
 
   vif
+}
+
+check_args_vif <- function(model, model_terms) {
+  if (length(class(model)) > 1) {
+    classes <- class(model)[class(model) %!in% c("lm", "lmerMod")]
+    msg <- glue::glue(
+      "If you would like it to be implemented, please file an issue at \\
+      https://github.com/verasls/lvmisc/issues."
+    )
+    abort_no_method_for_class("vif", classes, msg)
+  }
+  if (length(model_terms) < 2) {
+    rlang::abort(
+      "Not enought terms in `model` to check for multicollinearity."
+    )
+  }
 }
 
 has_intercept <- function(model) {
