424a44fd615fc52c3ad709f7c2e1df23475b2977
Lucas <lucasdsveras@gmail.com>
2021-01-05 19:23:34 -0300

    Test plot_model()

---
+++ tests/testthat/test-plot_model.R
@@ -0,0 +1,7 @@
+test_that("output of plot_model() is stable", {
+  m1 <- lm(disp ~ mpg, mtcars)
+  m2 <- lm(disp ~ mpg + hp + cyl + mpg:cyl, mtcars)
+
+  vdiffr::expect_doppelganger("Plot model - m1", plot_model(m1))
+  vdiffr::expect_doppelganger("Plot model - m2", plot_model(m2))
+})
